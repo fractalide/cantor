38f25721a264e1e9587ae902e9196c50514a48a9
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-05-22 15:20:45 +0200

    Problem: no Travis tests
    Solution: Just run nix-build.

---
+++ support/utils/nix-build-travis-fold.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --quiet -p bash coreutils gawk -i bash
+set -e
+set -u
+set -o pipefail
+
+function subfold() {
+  local prefix=$1
+  awk '
+    BEGIN {
+      date_cmd="date +%s%N"
+      current_scope="'$prefix'"
+      printf "travis_fold:start:%s\r", current_scope
+      printf "travis_time:start:%s\r", current_scope
+      date_cmd | getline start_time
+      close(date_cmd)
+    }
+    /^building \x27\/nix\/store\/.*[.]drv\x27/ {
+      date_cmd | getline finish_time
+      close(date_cmd)
+      printf "travis_time:end:%s:start=%s,finish=%s,duration=%s\r", \
+        current_scope, start_time, finish_time, (finish_time - start_time)
+      printf "travis_fold:end:%s\r", current_scope
+      current_scope=$0
+      sub("building \x27/nix/store/", "", current_scope)
+      sub("\x27.*", "", current_scope)
+      current_scope=current_scope ".." "'$prefix'"
+      printf "travis_fold:start:%s\r", current_scope
+      printf "travis_time:start:%s\r", current_scope
+      date_cmd | getline start_time
+      close(date_cmd)
+    }
+    { print }
+    END {
+      date_cmd | getline finish_time
+      close(date_cmd)
+      printf "travis_time:end:%s:start=%s,finish=%s,duration=%s\r", \
+        current_scope, start_time, finish_time, (finish_time - start_time)
+      printf "travis_fold:end:%s\r", current_scope
+    }
+  '
+}
+
+nix-build "$@" |& subfold ${!#}
