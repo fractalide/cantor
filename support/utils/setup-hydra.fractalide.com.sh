7de19d268a6eacb13a36109c5d22ed30d3b7b4bf
Stewart Mackenzie <setori88@gmail.com>
2018-06-08 05:23:45 +0800

    Problem: cardano-sl takes a very long time to build
    Solution: pull cardano-sl artefacts from hydra.iohk.io to
    speed up the builds

--- support/utils/setup-hydra.fractalide.com.sh
+++ support/utils/setup-hydra.fractalide.com.sh
@@ -8,7 +8,8 @@ set -o pipefail
 nix_conf=/etc/nix/nix.conf
 fracta_url=https://hydra.fractalide.com/
 fracta_key="hydra.fractalide.com-1:EnGZBrRHPabgRET+umo2e+wNrw+c4QdbsxsnV5H7zs0="
-
+iohk_url=https://hydra.iohk.io
+iohk_key="hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
 nixos_url=https://cache.nixos.org/
 nixos_key="cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
@@ -17,9 +18,9 @@ touch $nix_conf
 {
   grep -vE 'substituters|public-keys|require-sigs' $nix_conf || true
   cat <<EOF
-substituters = $nixos_url $fracta_url
-trusted-substituters = $nixos_url $fracta_url
-trusted-public-keys = $nixos_key $fracta_key
+substituters = $nixos_url $fracta_url $iohk_url
+trusted-substituters = $nixos_url $iohk_url $fracta_url
+trusted-public-keys = $nixos_key $iohk_key $fracta_key 
 EOF
 } > ${nix_conf}.new
 mv ${nix_conf}.new $nix_conf
